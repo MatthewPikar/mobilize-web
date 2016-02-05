5cfca723a6d8a4a7dd221cd78aebcc90e955cac4
matthew.pikar <matt.piekarczyk@cortixsystems.com>
2016-02-05 14:49:22 -0500

    fixed a bug in controller definition that was preventing the directive from being minified

--- app/layout/moreDirective.js
+++ app/layout/moreDirective.js
@@ -1,8 +1,32 @@
 'use strict'
 
+function moreController($scope){
+    $scope.toggleText = '...less'
+    $scope.text = ''
+
+    $scope.$watch('content', function(){
+        $scope.toggle($scope.content, $scope.length)
+    })
+
+    $scope.toggle = function (string, length){
+        if (string.length <= length) {
+            $scope.toggleText = ''
+            return $scope.text = string
+        }
+        else if($scope.toggleText === '...less' || $scope.toggleText === '') {
+            $scope.toggleText = '...more'
+            return $scope.text = string.slice(0,length)
+        }
+        else if($scope.toggleText === '...more' || $scope.toggleText === ''){
+            $scope.toggleText = '...less'
+            return $scope.text = string
+        }
+    }
+}
+
 var moreDirective = angular.module('more', [])
 
-moreDirective.directive('more', [
+moreDirective.directive('more',
     function() {
         return {
             restrict: 'E',
@@ -10,29 +34,6 @@ moreDirective.directive('more', [
             replace: true,
             scope: {length:'@', content:'@'},
             template: '<ng-transclude></ng-tranclude>',
-            controller: function ($scope){
-                $scope.toggleText = '...less'
-                $scope.text = ''
-
-                $scope.$watch('content', function(){
-                    $scope.toggle($scope.content, $scope.length)
-                })
-
-                $scope.toggle = function (string, length){
-                    if (string.length <= length) {
-                        $scope.toggleText = ''
-                        return $scope.text = string
-                    }
-                    else if($scope.toggleText === '...less' || $scope.toggleText === '') {
-                        $scope.toggleText = '...more'
-                        return $scope.text = string.slice(0,length)
-                    }
-                    else if($scope.toggleText === '...more' || $scope.toggleText === ''){
-                        $scope.toggleText = '...less'
-                        return $scope.text = string
-                    }
-                }
-            }
+            controller: ["$scope",moreController]
         }
-
-}])
+})
