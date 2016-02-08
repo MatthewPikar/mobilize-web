d2454e017b88aba628d0c2c441b52ff82563d9ff
matthew.pikar <matt.piekarczyk@cortixsystems.com>
2016-02-08 18:37:16 -0500

    fixed query to pass along the query variable

--- app/events/eventsService.js
+++ app/events/eventsService.js
@@ -4,22 +4,26 @@ var eventsService = angular.module('eventsService', ['ngResource']);
 
 eventsService.factory('Event', ['$resource',
     function($resource){
-        return $resource(API_PATH + 'events/:id', {id:'@id'}, {
-            add: {
-                method:'POST',
-                interceptor: { response: function(response){ return response.data.resources[0] } }
-            },
-            modify:{
-                method:'PUT',
-                interceptor: { response: function(response){ return response.data.resource } }
-            },
-            query:{
-                method:'GET',
-                interceptor: { response: function(response){ return response.data.resources } }
-            },
-            get: {
-                method: 'GET',
-                interceptor: { response: function(response){ return response.data.resource } }
-            }
+        return $resource(API_PATH + 'events/:id', { id:'@id' },
+            {
+                add: {
+                    method:'POST',
+                    interceptor: { response: function(response){ return response.data.resources[0] } }
+                },
+                modify:{
+                    method:'PUT',
+                    interceptor: { response: function(response){ return response.data.resource } }
+                },
+                query:{
+                    method:'GET',
+                    params: { query: '@query' },
+                    interceptor: { response: function(response){
+                        console.log(response)
+                        return response.data.resources } }
+                },
+                get: {
+                    method: 'GET',
+                    interceptor: { response: function(response){ return response.data.resource } }
+                }
         });
     }]);
