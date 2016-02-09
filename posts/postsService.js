cd5dd2e2bb4b4d937a50e31ed1126e8baa0e4939
matthew.pikar <matt.piekarczyk@cortixsystems.com>
2016-02-09 12:49:23 -0500

    first version

---
+++ posts/postsService.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var postsService = angular.module('postsService', ['ngResource']);
+
+postsService.factory('Event', ['$resource',
+    function($resource){
+        return $resource(API_PATH + 'events/:id', { id:'@id' },
+            {
+                add: {
+                    method:'POST',
+                    interceptor: { response: function(response){ return response.data.resources[0] } }
+                },
+                modify:{
+                    method:'PUT',
+                    interceptor: { response: function(response){ return response.data.resource } }
+                },
+                query:{
+                    method:'GET',
+                    params: { query: '@query' },
+                    interceptor: { response: function(response){ return response.data.resources } }
+                },
+                get: {
+                    method: 'GET',
+                    interceptor: { response: function(response){ return response.data.resource } }
+                }
+        });
+    }]);
