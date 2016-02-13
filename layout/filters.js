266c9ed4fe5dec73b55bc4093bee47768c8070f6
matthew.pikar <matt.piekarczyk@cortixsystems.com>
2016-02-12 19:57:21 -0500

    Added capitalize filter

---
+++ layout/filters.js
@@ -0,0 +1,17 @@
+'use strict'
+
+function capitalize (input) {
+    var output = []
+    if (input) {
+        var words = input.split(' ')
+
+        for (var w= 0, len=words.length; w<len; w++){
+            output[w] = words[w].charAt(0).toUpperCase() + words[w].substr(1)
+        }
+    }
+    return output.join(' ')
+}
+
+var filters = angular.module('filters',[])
+
+filters.filter('capitalize', function () { return capitalize })
