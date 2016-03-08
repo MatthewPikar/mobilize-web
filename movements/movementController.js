5ff4013f561727cc48d044fe0319f337473ea0af
matthew.pikar <matt.piekarczyk@cortixsystems.com>
2016-03-08 15:16:59 -0500

    simplified styling, added actions

--- movements/movementController.js
+++ movements/movementController.js
@@ -2,16 +2,57 @@
 
 var movementController = angular.module('movementController', [])
 
-movementController.controller('movementController', ['$scope','$state','LocalState','$uibModal','$sce','movement','posts','events',
-function($scope, $state, LocalState, $uibModal, $sce, movement,posts,events) {
+movementController.controller('movementController', ['$scope','$state','LocalState','$uibModal','$sce',
+    'movement','posts','events','actions',
+function($scope, $state, LocalState, $uibModal, $sce, movement, posts, events, actions) {
+    $scope.actionTypes = {
+        Share: {
+            active: false,
+            label: 'Share',
+            icon: 'fa-bullhorn',
+            description: 'Your message will be shared on social media on behalf of members.',
+            fields: ['message']
+        },
+        Mail: {
+            active: false,
+            label: 'Mail',
+            icon: 'fa-envelope-o',
+            description: 'Provide a letter, which will be signed and sent on behalf of members.',
+            helpText: '',
+            fields: ['targets', 'message']
+        },
+        'E-mail': {
+            active: false,
+            label: 'E-mail',
+            icon: 'fa-at',
+            description: 'Provide an e-mail, which will be signed and sent on behalf of members.',
+            fields: ['targets', 'message']
+        },
+        Petition: {
+            active: false,
+            label: 'Petition',
+            icon: 'fa-clipboard',
+            description: 'Provide a petition, which will be signed on behalf of members.',
+            fields: ['targets', 'message']
+        },
+        Call: {
+            active: false,
+            label: 'Call',
+            icon: 'fa-phone',
+            description: 'Provide the talking points and members will pledge to call.',
+            fields: ['targets', 'instructions']
+        }
+    }
 
     // Initialization
+    $scope.masterState = $state
     $scope.now = new Date(Date.now())
     $scope.movement = movement
     $scope.sourceId = $scope.movement.id
     $scope.videoResource = $sce.trustAsResourceUrl($scope.movement.video)
     $scope.posts = posts
     $scope.events = events
+    $scope.actions = actions
 
     if (events) {
         for (var e = 0, len = $scope.events.length; e < len; e++) {
